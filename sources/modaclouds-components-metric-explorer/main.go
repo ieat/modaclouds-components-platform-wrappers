eb5107346b6d2a8a18f5244788637e41d307c590
Ciprian Dorin Craciun <ciprian@volution.ro>
2014-10-09 11:42:52 +0300

    Import initial code

---
+++ sources/modaclouds-components-metric-explorer/main.go
@@ -0,0 +1,78 @@
+
+
+package main
+
+
+import "errors"
+import "fmt"
+import "net"
+import "os"
+
+import "vgl/transcript"
+
+import . "mosaic-components/examples/simple-server"
+import . "mosaic-components/libraries/messages"
+
+
+var selfGroup = ComponentGroup ("7e079e121d36b8cec279b3116b2ec3d9a4e36045")
+
+
+type callbacks struct {
+	httpIp net.IP
+	httpPort uint16
+	httpFqdn string
+}
+
+
+func (_callbacks *callbacks) Initialize (_server *SimpleServer) (error) {
+	
+	_server.Transcript.TraceInformation ("acquiring the HTTP endpoint...")
+	if _ip_1, _port_1, _fqdn_1, _error := _server.TcpSocketAcquire (ResourceIdentifier ("http")); _error != nil {
+		return _error
+	} else {
+		_callbacks.httpIp = _ip_1
+		_callbacks.httpPort = _port_1
+		_callbacks.httpFqdn = _fqdn_1
+	}
+	
+	_server.Transcript.TraceInformation ("  * using the HTTP endpoint: `%s:%d`;", _callbacks.httpIp.String (), _callbacks.httpPort)
+	
+	_server.ProcessExecutable = os.Getenv ("modaclouds_metric_explorer_run")
+	_server.ProcessEnvironment = map[string]string {
+			"MODACLOUDS_METRIC_EXPLORER_ENDPOINT_IP" : _callbacks.httpIp.String (),
+			"MODACLOUDS_METRIC_EXPLORER_ENDPOINT_PORT" : fmt.Sprintf ("%d", _callbacks.httpPort),
+	}
+	_server.SelfGroup = selfGroup
+	
+	return nil
+}
+
+
+func (_callbacks *callbacks) Called (_server *SimpleServer, _operation ComponentOperation, _inputs interface{}) (_outputs interface{}, _error error) {
+	
+	switch _operation {
+		
+		case "modaclouds-metric-explorer:get-http-endpoint" :
+			
+			_outputs = map[string]interface{} {
+					"ip" : _callbacks.httpIp.String (),
+					"port" : _callbacks.httpPort,
+					"fqdn" : _callbacks.httpFqdn,
+					"url" : fmt.Sprintf ("http://%s:%d/", _callbacks.httpFqdn, _callbacks.httpPort),
+			}
+			
+		default :
+			
+			_error = errors.New ("invalid-operation")
+	}
+	
+	return _outputs, _error
+}
+
+
+func main () () {
+	PreMain (& callbacks {}, packageTranscript)
+}
+
+
+var packageTranscript = transcript.NewPackageTranscript ()
