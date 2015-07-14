09b035cc537b236afa1aaac5ccdc20a70bfa5103
Ciprian Dorin Craciun <ciprian@volution.ro>
2015-07-14 14:24:44 +0300

    Rename `knowledgebase` to `fuseki`

--- modaclouds-components-fuseki/sources/modaclouds-components-knowledgebase/main.go
+++ modaclouds-components-fuseki/sources/modaclouds-components-knowledgebase/main.go
@@ -37,7 +37,7 @@ func (_callbacks *callbacks) Initialize (_server *SimpleServer) (error) {
 	
 	_server.Transcript.TraceInformation ("  * using the HTTP endpoint: `%s:%d`;", _callbacks.httpIp.String (), _callbacks.httpPort)
 	
-	_server.ProcessExecutable = os.Getenv ("modaclouds_knowledgebase_run")
+	_server.ProcessExecutable = os.Getenv ("modaclouds_fuseki_run")
 	_server.ProcessEnvironment = map[string]string {
 			"MODACLOUDS_KNOWLEDGEBASE_ENDPOINT_IP" : _callbacks.httpIp.String (),
 			"MODACLOUDS_KNOWLEDGEBASE_ENDPOINT_PORT" : fmt.Sprintf ("%d", _callbacks.httpPort),
@@ -54,7 +54,7 @@ func (_callbacks *callbacks) Called (_server *SimpleServer, _operation Component
 	
 	switch _operation {
 		
-		case "modaclouds-knowledgebase:get-http-endpoint" :
+		case "modaclouds-fuseki:get-http-endpoint" :
 			
 			_outputs = map[string]interface{} {
 					"ip" : _callbacks.httpIp.String (),
