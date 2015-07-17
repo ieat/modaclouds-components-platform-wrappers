3d7f15b1ad0e0921ca14e269a0191a5dc6f1cd6d
Ciprian Dorin Craciun <ciprian@volution.ro>
2015-07-17 13:22:34 +0300

    Minor fixup

--- modaclouds-components-fuseki/sources/modaclouds-components-fuseki/main.go
+++ modaclouds-components-fuseki/sources/modaclouds-components-fuseki/main.go
@@ -39,8 +39,8 @@ func (_callbacks *callbacks) Initialize (_server *SimpleServer) (error) {
 	
 	_server.ProcessExecutable = os.Getenv ("modaclouds_fuseki_run")
 	_server.ProcessEnvironment = map[string]string {
-			"MODACLOUDS_KNOWLEDGEBASE_ENDPOINT_IP" : _callbacks.httpIp.String (),
-			"MODACLOUDS_KNOWLEDGEBASE_ENDPOINT_PORT" : fmt.Sprintf ("%d", _callbacks.httpPort),
+			"MODACLOUDS_FUSEKI_ENDPOINT_IP" : _callbacks.httpIp.String (),
+			"MODACLOUDS_FUSEKI_ENDPOINT_PORT" : fmt.Sprintf ("%d", _callbacks.httpPort),
 			"modaclouds_service_identifier" : string (_server.Identifier),
 			"modaclouds_service_temporary" : fmt.Sprintf ("%s/service", _server.Temporary),
 	}
